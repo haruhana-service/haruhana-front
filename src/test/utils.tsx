fd3a871e5ffb57b22ff00644118c4e5637d7a3c2
kwakseobang <iii6602@naver.com>
2026-02-16 10:15:27 +0900

    chore(tooling): ESLint 설정 및 테스트 유틸리티 업데이트

--- src/test/utils.tsx
+++ src/test/utils.tsx
@@ -4,7 +4,7 @@ import { BrowserRouter } from 'react-router-dom'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { AuthProvider } from '../contexts/AuthContext'
 
-// Create a custom render function that includes all providers
+// eslint-disable-next-line react-refresh/only-export-components
 function AllTheProviders({ children }: { children: React.ReactNode }) {
   const queryClient = new QueryClient({
     defaultOptions: {
@@ -28,5 +28,6 @@ function customRender(ui: ReactElement, options?: Omit<RenderOptions, 'wrapper'>
 }
 
 // Re-export everything
+// eslint-disable-next-line react-refresh/only-export-components
 export * from '@testing-library/react'
 export { customRender as render }
